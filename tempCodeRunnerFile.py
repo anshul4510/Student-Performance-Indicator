f2b7d7bb428734c6418b994fe30a83629a5c7995
anshul_1905 <anshul4510@users.noreply.github.com>
2025-08-27 21:43:06 +0530

    css

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,7 @@
+ gender=request.form.get('gender'),
+            race_ethnicity=request.form.get('ethnicity'),
+            parental_level_of_education=request.form.get('parental_level_of_education'),
+            lunch=request.form.get('lunch'),
+            test_preparation_course=request.form.get('test_preparation_course'),
+            reading_score=float(request.form.get('writing_score')),
+            writing_score=float(request.form.get('reading_score'))
