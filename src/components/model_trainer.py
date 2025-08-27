f2e8a39bb3e246a66ba2964cbec5eb89d03f5607
anshul_1905 <anshul4510@users.noreply.github.com>
2025-08-27 15:22:38 +0530

    pipeline

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -84,7 +84,7 @@ class ModelTrainer:
                 
             }
 
-            model_report:dict=evaluate_models(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,models=models,param=params)
+            model_report:dict=evaluate_models(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,models=models,param`=params)
             best_model_score=max(sorted(model_report.values()))
             best_model_name=list(model_report.keys())[
                 list(model_report.values()).index(best_model_score)
--- src/utils.py
+++ src/utils.py
@@ -36,3 +36,13 @@ def evaluate_models(X_train,y_train,X_test,y_test,models,param):
         return report
     except Exception as e:
         raise CustomException(e,sys)
+
+
+def load_object(file_path):
+    try:
+        if not os.path.exists(file_path):
+            raise FileNotFoundError(f"File not found: {file_path}")
+        with open(file_path, 'rb') as file_obj:
+            return dill.load(file_obj)
+    except Exception as e:
+        raise CustomException(e, sys)
