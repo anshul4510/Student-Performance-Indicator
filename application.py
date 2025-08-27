28f230ec5efc8045a69409befe8884f304802cae
anshul_1905 <anshul4510@users.noreply.github.com>
2025-08-27 20:40:28 +0530

    deployment_trial_1

--- application.py
+++ application.py
@@ -30,7 +30,3 @@ def predict_datapoint():
         predict_pipeline=PredictPipeline()
         result=predict_pipeline.predict(pred_df)
         return render_template('home.html',result=result[0])
-
-
-if __name__=="__main__":
-    app.run(host="0.0.0.0")
